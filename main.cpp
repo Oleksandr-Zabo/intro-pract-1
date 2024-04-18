63ba84877731a3dc39a44a70c6496f7feccbd180
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-18 20:40:49 +0300

    ex.3 lab1 have done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 int main()
 {
-	cout << "/Bjarne Stroustrup/" << endl;
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "To be,! ";
+
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << "or not ";
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "to be: that is the question: "<<endl<< "Whether ";
+
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "\'tis nobler in the mind to suffer " << endl;
+	cout << "The slings and arrows of outrageous fortune," << endl;
+	cout << "Or to take arms against a sea of troubles," << endl;
+	cout << "And by opposing end them?" << endl;
+	cout << "William Shakespeare" << endl;
+	
 	system("pause");
 	return 0;
 }
