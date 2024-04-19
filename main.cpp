1a4ff11cf90decf4beef29ef2dc28d5bbe87b953
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-19 20:43:48 +0300

    ex. 2 hw had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 int main()
 {
-	cout << "/Bjarne Stroustrup/" << endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	SetConsoleTextAttribute(hConsole, 0x04);
+	cout << "Every"<<endl;
+
+	SetConsoleTextAttribute(hConsole, 0x0C);
+	cout << "\tHunter" << endl;
+
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "\t\tWants" << endl;
+
+	SetConsoleTextAttribute(hConsole, 0x02);
+	cout << "\t\t\tTo know" << endl;
+
+	SetConsoleTextAttribute(hConsole, 0x09);
+	cout << "\t\t\t\tWhere" << endl;
+	
+	SetConsoleTextAttribute(hConsole, 0x01);
+	cout << "\t\t\t\t\tSits" << endl;
+	
+	SetConsoleTextAttribute(hConsole, 0x0D);
+	cout << "\t\t\t\t\t\tPheasant" << endl;
+
+	SetConsoleTextAttribute(hConsole, 0x0A);
 	system("pause");
 	return 0;
 }
