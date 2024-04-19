5cc3d8e51cf07e2975b2626dd1209c5e226815ef
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-19 21:13:15 +0300

    ex.3  hw had done

--- main.cpp
+++ main.cpp
@@ -3,13 +3,48 @@
 using namespace std;
 int main()
 {
-
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 0x07);
+
+	cout << "Продам квіти." << endl;
+	cout << "Вид: Фіалка.\n" << endl;
+
+	cout << "(...)Фіалка – чи не найпопулярніша кімнатна квітка. \nДля фіалки підійдуть вікна, \nякі \"дивляться\" на північний схід, \nпівніч та північний захід." << endl;
+
+	cout << "\nТел.: 222-22-22" << endl;
+
+	cout <<"222\t";
+	cout <<"|\t";
+
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "222\t";
 
 	SetConsoleTextAttribute(hConsole, 0x07);
-	cout << "Продам квіти." << endl;
-	cout << "Вид Фіалка" << endl;
+	cout << "|\n";
+
+	cout << "22 \t";
+	cout << "|\t";
+
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "22 \t";
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "|\n";
+
+	cout << "22 \t";
+	cout << "|\t";
+
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "22 \t";
+
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "|\n";
+
+	cout << "і т.д."<<endl;
+
 	system("pause");
 	return 0;
 }
