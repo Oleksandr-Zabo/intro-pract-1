ebab45037b88a04d98bccef494be3f8b10e2ba49
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-19 19:45:35 +0300

    ex.1 hw intro had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 int main()
 {
-	cout << "/Bjarne Stroustrup/" << endl;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Як умру, то поховайте" <<endl;
+	cout << "Мене на могилі," << endl;
+	cout << "Серед степу широкого," << endl;
+	cout << "На Вкраїні милій," << endl;
+	cout << "Щоб лани широкополі," << endl;
+	cout << "І Дніпро, і кручі" << endl;
+	cout << "Було видно, було чути," << endl;
+	cout << "Як реве ревучий." << endl;
+	cout << "\nТарас Шевченко\n" << endl;
 	system("pause");
 	return 0;
 }
